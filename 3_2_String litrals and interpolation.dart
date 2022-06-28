c98fb49457bb9fe529252e728eabe1baecdc946b
dathrob <dev.misganaw.meseret@gmail.com>
2022-06-28 21:16:32 +0300

    string literals and interpolation

---
+++ 3_2_String litrals and interpolation.dart
@@ -0,0 +1,17 @@
+void main() {
+//Literals are to be assigned to variables
+
+//various ways to define String Literals in Dart
+  String s1 = 'Single';
+  String s2 = "Double";
+  String noplus = 'this is the first string '
+      'thiis is the second string';
+  //now on dart we donot need the plus sign to concatinate strings
+//String interpolation
+  String name = "Misganaw";
+  String message = "my name is $name";
+
+  print("The length of my name is ${name.length}");
+
+//Interpolation is applicable on int and double
+}
