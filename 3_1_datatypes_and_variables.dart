9c2f4b3299717ba3bca00563a6748e38cdeadfe8
dathrob <dev.misganaw.meseret@gmail.com>
2022-06-28 20:51:07 +0300

    3_1 datatypes

---
+++ 3_1_datatypes_and_variables.dart
@@ -0,0 +1,24 @@
+//All data types in Dart are Objects
+//Therefore, values are by default null
+
+void main() {
+  var today = 27;
+
+//Numbers int
+  int hexValue = 0xEADEBAEE; //we can assign hex value as long as the valu
+//after the x is 8 digit
+  int age = 10;
+
+//Numbers:double
+  double height = 174.5;
+  double exponents = 1.4e5; //we can assign exponential value to doubles
+
+//Strings
+  String name = "Misganaw";
+
+//boolean
+  bool isAlive = true;
+
+  print(hexValue);
+  print(exponents);
+}
--- helloworld.dart
+++ helloworld.dart
@@ -1,4 +1,8 @@
 void main() {
+  //this is a comment on dart
   print("hello world");
+
   print("this is my first app");
+  print("yishak wondim");
+  /*this is a multiline comment  */
 }
