130eaa6c213c6f3b01946b1dc513159aafec59f1
dathrob <dev.misganaw.meseret@gmail.com>
2022-07-01 17:30:54 +0300

    loop controls

---
+++ 5_1 Loop control.dart
@@ -0,0 +1,32 @@
+void main() {
+  //Loop Structure
+
+  //Counter variable
+  //Condition check
+  //Increament or Decrement the counter
+
+  //definite loops
+  //    if you know how many times your loop should be executed use For
+  //indefinite loops
+  //    if you want to execute a code based on some condition use while/dowhile
+
+  //Difference between while and dowhile loop
+
+  //for loop
+
+  for (var i = 0; i < 4; i++) {
+    print("hello from for loop");
+  }
+  var i = 0;
+  //while loop -check the condition at the start
+  while (i < 4) {
+    print("Hello for while loop");
+    i++;
+  }
+  var j = 0;
+  //do while loop -check the condition at the end
+  do {
+    print("hello from do while loop");
+    j++;
+  } while (j < 4);
+}
