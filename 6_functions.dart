58dc9b5582161f1b798fdaa82654830154e78059
dathrob <dev.misganaw.meseret@gmail.com>
2022-07-02 19:47:05 +0300

    optional parameters

--- 6_functions.dart
+++ 6_functions.dart
@@ -3,6 +3,7 @@ void main() {
   int area = findArea(10, 5);
   print("the area of the rectangle is $area");
   fatarrow(10);
+  countries("Ethiopia");
   //dart returns null for no return functions
 }
 
@@ -19,3 +20,10 @@ int findArea(int length, int breadth) {
 // in between the fat arrow and the semicolon we can only have
 // a single expression
 void fatarrow(int length) => length * length;
+
+//optional parameters
+
+/*void countries(String name1, [String name2, String name3]) {
+  print("here is the value $name1,$name2, $name3");
+}'''*/
+//String cannot be null but this is how to make optional parameters
